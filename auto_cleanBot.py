e2fd9419b441725b8db533f7fee864be0b61a98e
nel951 <62973378+hainwoo@users.noreply.github.com>
2021-07-18 23:50:29 +0900

    크롬

--- auto_cleanBot.py
+++ auto_cleanBot.py
@@ -83,7 +83,7 @@ class s :
 def main() :
     global driver
     start = s
-    driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver.exe')
+    driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver',chrome_options='/app/.apt/usr/bin/google-chrome')
     driver.implicitly_wait(1)
     driver.get(URL_login)
     search = driver.find_element_by_css_selector('#btnConfirm2')
